f5b509ee74e2f2fdae5f3e3bfb9cf1e52941db30
LimerBoy <78836891+updateu3@users.noreply.github.com>
2024-03-26 19:54:57 +1100

    Delete Helpers directory

--- Helpers/AnimationEx.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DaddyRecoveryBuilder.Helpers
-{
-    using System;
-
-    public static class AnimationEx
-    {
-        public static bool Show(IntPtr hWnd, int time, Enums.AnimateWindowFlags flags) => NativeMethods.AnimateWindow(hWnd, time, flags);
-    }
-}
